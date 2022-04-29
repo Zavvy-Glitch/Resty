65b5f776b8f9286d5841df790879eef49fb53e34
Chattray <chattray.chea@gmail.com>
2022-04-28 17:52:52 -0700

    useEffect / useReduce Reworking

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./app.scss";
 
-// Let's talk about using index.js and some other name in the component folder
-// There's pros and cons for each way of doing this ...
+
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Form from "./components/form";
